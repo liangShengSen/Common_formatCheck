46747124d3bc4853feb0cd9c05f69f97bde168e9
liangshengsen <1213509006@qq.com>
2019-06-12 08:52:08 +0800

    cookie function

--- methodUtils.js
+++ methodUtils.js
@@ -48,3 +48,22 @@ export const getValue = (i,k,d) => {
     }
     return o;
 };
+// 根据key获取cookie的value
+export const getCookie = (c_name) => {
+    if (document.cookie.length > 0){
+        c_start = document.cookie.indexOf(c_name + "=")
+        if (c_start != -1){
+            c_start = c_start + c_name.length + 1
+            c_end = document.cookie.indexOf(";",c_start)
+            if (c_end == -1) c_end = document.cookie.length
+            return unescape(document.cookie.substring(c_start,c_end))
+        }
+    }
+    return null;
+};
+// 设置cookie
+export const setCookie = (c_name,value,expiredays) => {
+    let exdate = new Date();
+    exdate.setDate(exdate.getDate() + expiredays)
+    document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "" : "; expires=" + exdate.toUTCString())
+}
