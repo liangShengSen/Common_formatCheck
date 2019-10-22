63678a37993c5b13c2e82dc4c5770d1a71a3bdba
liangshengsen <1213509006@qq.com>
2019-10-22 14:14:56 +0800

    防抖&节流

--- methodUtils.js
+++ methodUtils.js
@@ -225,6 +225,34 @@ const methodsUtil = {
     return ![, null].includes(val) && val.constructor === type;
   },
 
+  // 节流
+  throttle(fn, gapTime) {
+    let _lastTime = null;
+    return function () {
+      let _nowTime = + new Date()
+      if (_nowTime - _lastTime > gapTime || !_lastTime) {
+        fn();
+        _lastTime = _nowTime
+      }
+    }
+  },
+
+  // 防抖:防止多次表单提交，减少请求次数;页面滚动
+  debounce(fn, wait) {
+    var timer = null;
+    return function () {
+        var context = this
+        var args = arguments
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+        timer = setTimeout(function () {
+            fn.apply(context, args)
+        }, wait)
+    }
+  }
+
 }
 
 module.exports = methodsUtil
